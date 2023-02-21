7da3106522969554fc5069f1ab4f2c1be96959d1
高橋友和 <takahashitomokazu@MacBook-Pro.local>
2023-02-21 12:02:06 +0900

    before update index.js,App.js

--- 5practice/my-app5/src/index.js
+++ 5practice/my-app5/src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from "react-dom";
-import {CssModules} from "./CssModules";
-import {StyledJsx} from "./StyledJsx";
+// import {CssModules} from "./CssModules";
+import { StyledComponents } from "./StyledComponents";
+// import {StyledJsx} from "./StyledJsx";
 
 
 // ReactDOM.render(<CssModules />, document.getElementById("root"));
-ReactDOM.render(<StyledJsx />, document.getElementById("root"));
+// ReactDOM.render(<StyledJsx />, document.getElementById("root"));
+ReactDOM.render(<StyledComponents />, document.getElementById("root"));
